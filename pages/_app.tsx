9419c812f24ac6d2a61d29d559885db783f03ac5
Fabian Ferno <fabianferno@gmail.com>
2023-08-11 01:05:46 +0530

    Fix rainbow kit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,12 @@ import type { AppProps } from "next/app";
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, optimismGoerli, zora } from "wagmi/chains";
+import { optimism, optimismGoerli, zora } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, optimismGoerli, zora],
+  [optimism, optimismGoerli, zora],
   [alchemyProvider({ apiKey: process.env.ALCHEMY_ID || "" }), publicProvider()]
 );
 const { connectors } = getDefaultWallets({
